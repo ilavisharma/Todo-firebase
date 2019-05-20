ded7068e0771390f1e41c8ebede60f14bee82344
ilavisharma <lavisharma796@gmail.com>
2019-05-20 16:54:29 +0530

    basic setup

---
+++ features.md
@@ -0,0 +1,6 @@
+## Features to be added
+
+- [ ] display todos ui
+- [ ] delete todos
+- [ ] Authentication via Firebase auth
+- [ ] Store the todos in firebase
---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div id="container">
+      <h1>To-Do List</h1>
+    </div>
+  );
+};
+
+export default App;
