6d363895f91b0062316eb979bd4ce0914962048c
ilavisharma <lavisharma796@gmail.com>
2019-05-20 18:44:54 +0530

    firestore read working

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import TodoList from './TodoList';
+import firebase from '../lib/firebase';
 
 const App = () => {
+  const [todos, updateTodos] = useState([]);
+  const db = firebase.firestore();
+
+  useEffect(() => {
+    db.collection('todos')
+      .get()
+      .then(snapshot => {
+        snapshot.forEach(doc => {
+          const { todos } = doc.data();
+          updateTodos(todos);
+        });
+      })
+      .catch(err => {
+        console.log('Error getting documents', err);
+      });
+  }, []);
+
   return (
     <div>
       <h1>To-Do List</h1>
-      <TodoList todos={['a', 'b', 'c']} />
+      <TodoList todos={todos} />
     </div>
   );
 };
