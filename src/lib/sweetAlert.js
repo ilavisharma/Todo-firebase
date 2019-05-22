d1b84769b960744ee3aef33484eeaa5d30aa09c0
ilavisharma <lavisharma796@gmail.com>
2019-05-22 19:32:42 +0530

    swwet alert mixin config

---
+++ src/lib/sweetAlert.js
@@ -0,0 +1,13 @@
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+
+const MySwal = withReactContent(Swal);
+
+const Toast = MySwal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 2500
+});
+
+export default Toast;
